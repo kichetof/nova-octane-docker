35a3dc8693820d23f0465920cf48b4300d9214d8
Christophe Francey <kichetof@users.noreply.github.com>
2025-04-06 10:09:51 +0200

    Install Laravel-Lang + add FR translation (sail artisan lang:add fr)

---
+++ lang/vendor/nova/fr/validation.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'attached'  => ':Attribute est déjà attaché(e).',
+    'relatable' => ':Attribute n\'est sans doute pas associé(e) avec cette donnée.',
+];
